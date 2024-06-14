0cb00722ed0383e8d51fe7aed3b4876109ccbf2e
pav9551 <pav9551@yandex.ru>
2024-06-14 18:31:57 +0300

    added comment of string

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ from hello import Role , User
 
 with app.app_context():
     db.create_all()
-    admin_role = Role(name = 'Admin')
-    db.session.add(admin_role)
-    db.session.commit()
-    user_john = User(username = 'John', role = admin_role)
-    db.session.add(user_john)
-    db.session.commit()
+    #admin_role = Role(name = 'Admin')
+    #db.session.add(admin_role)
+    #db.session.commit()
+    #user_john = User(username = 'John', role = admin_role)
+    #db.session.add(user_john)
+    #db.session.commit()
 
 
